e2d3635e1c99b753f8b3a700ee9da796173efc0b
Devin Siems <dms.gamer@gmail.com>
2017-10-10 18:13:41 -0700

    Create Assignment 1 Git

---
+++ app/src/main/java/siems/devin/assignment1/MainActivity.java
@@ -0,0 +1,23 @@
+package siems.devin.assignment1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);Button button = (Button)findViewById(R.id.enter_info_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EnterInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
