5959a2f5dad115b6a36d58a2c5d986732d81cf44
Devin Siems <dms.gamer@gmail.com>
2017-10-11 16:22:11 -0700

    modified view activity to add scolling and amd attempting to only use one text view form each line by using append()

--- app/src/main/java/siems/devin/assignment1/ViewInfoActivity.java
+++ app/src/main/java/siems/devin/assignment1/ViewInfoActivity.java
@@ -10,12 +10,17 @@ public class ViewInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_info);
+
         TextView photoNameTextView = (TextView)findViewById(R.id.photoName);
         TextView photographerTextView = (TextView)findViewById(R.id.photographer);
         TextView photoYearTextView = (TextView)findViewById(R.id.photoYear);
 
-        photoNameTextView.setText(EnterInfoActivity.photoName);
-        photographerTextView.setText(EnterInfoActivity.photographer);
-        photoYearTextView.setText(EnterInfoActivity.photoYear);
+        photoNameTextView.setText(getString(R.string.photo_name));
+        photographerTextView.setText(getString(R.string.photographer_name));
+        photoYearTextView.setText(getString(R.string.photo_year_taken));
+
+        photoNameTextView.append(EnterInfoActivity.photoName);
+        photographerTextView.append(EnterInfoActivity.photographer);
+        photoYearTextView.append(EnterInfoActivity.photoYear);
     }
 }
